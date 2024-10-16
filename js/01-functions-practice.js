71a3d446be39e56f57f115b9c6c72b20b099dee2
Brittany Anderson <142555405+brittanyanderson1@users.noreply.github.com>
2024-10-16 16:29:26 -0700

    Exercise 2 for practice with functions completed.

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -4,7 +4,11 @@ function halfNumber(num) {
     console.log(`${num} divided by 2 is the same as ${result} is half of ${num}`)
 }
 //STEP 2
-
+function squareNumber(num) {
+    let a = num
+    let square = num ** 2
+    console.log(`${square} is ${num} squared`) 
+}
 //STEP 3
 
 //STEP 4
