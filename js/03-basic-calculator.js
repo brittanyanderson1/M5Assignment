b97dee888e8aec622fe246b1bbf17a225ea3ef54
Brittany Anderson <142555405+brittanyanderson1@users.noreply.github.com>
2024-10-24 20:31:39 -0700

    Final Commit

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,46 +1,47 @@
 // ADD A FUNCTION CALLED CALCULATE
 function calculate(x, y, operator) {
     if (isNaN(x) || isNaN(y)) {
-        alert('Invalid entry. Please enter a valid number.');
-        return null;
+        alert('Invalid entry. Please enter a valid number.')
+        return null
     }
     
-    let result;
+    let result
+
     if (operator === '+') {
-        result = x + y;
+        result = x + y
     } else if (operator === '-') {
-        result = x - y;
+        result = x - y
     } else if (operator === '*') {
-        result = x * y;
+        result = x * y
     } else if (operator === '/') {
         if (y === 0) {
-            alert('Cannot divide by zero.');
-            return null;
+            alert('Cannot divide by zero.')
+            return null
         }
-        result = x / y;
+        result = x / y
     } else {
-        alert('Invalid operator. Please enter a valid operator (+, -, *, /).');
-        return null;
+        alert('Invalid operator. Please enter a valid operator (+, -, *, /).')
+        return null
     }
-    return result;
+    return result
 }
 // COLLECT FIRST NUMBER FROM USER
-let x = parseFloat(prompt('Enter a number to calculate'));
+let x = parseFloat(prompt('Enter a number to calculate'))
 if (isNaN(x)) {
-    alert('Invalid entry. Please enter a valid number.');
+    alert('Invalid entry. Please enter a valid number.')
 } else {
 // COLLECT SECOND NUMBER FROM USER
-    let y = parseFloat(prompt('Enter a second number to calculate'));
+    let y = parseFloat(prompt('Enter a second number to calculate'))
     if (isNaN(y)) {
-        alert('Invalid entry. Please enter a valid number.');
+        alert('Invalid entry. Please enter a valid number.')
     } else {
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
-        let operator = prompt('Enter the mathematical operator you want to perform (+, -, *, /)');
+        let operator = prompt('Enter the mathematical operator you want to perform (+, -, *, /)')
 // CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
-        let result = calculate(x, y, operator);
+        let result = calculate(x, y, operator)
         if (result !== null) {
-            alert(`The  result  of  ${x}  ${operator}  ${y} is  ${result}`);
-            console.log(result);
+            alert(`The  result  of  ${x}  ${operator}  ${y} is  ${result}`)
+            console.log(result)
         }
     }
 }
